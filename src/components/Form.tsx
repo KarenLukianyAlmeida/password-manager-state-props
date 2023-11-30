aa1bf19c01ea2920fbb14227bb0efd4ed35df2b1
Karen Almeida <karenalmeida.lukiany@gmail.com>
2023-11-30 19:01:42 +0000

    Cria requisito 3, aprova

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,46 +1,69 @@
-// const initialRegister = {
-//   service: '';
-//   login: '';
-// }
+import { useState } from 'react';
 
 function Form() {
+  const [newRegister, setNewRegister] = useState(false);
+  const [showButton, setShowButton] = useState(true);
+
+  function showForm(event: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
+    event.preventDefault();
+    setNewRegister(true);
+    setShowButton(false);
+  }
+
+  function cancelForm(event: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
+    event.preventDefault();
+    setNewRegister(false);
+    setShowButton(true);
+  }
+
   return (
-    <form>
-      <div>
-        <label htmlFor="service">Nome do serviço</label>
-        <input
-          type="text"
-          name="service"
-          id="service"
-        />
-      </div>
-      <div>
-        <label htmlFor="login">Login</label>
-        <input
-          type="text"
-          name="login"
-          id="login"
-        />
-      </div>
-      <div>
-        <label htmlFor="senha">Senha</label>
-        <input
-          type="password"
-          name="senha"
-          id="senha"
-        />
-      </div>
-      <div>
-        <label htmlFor="url">URL</label>
-        <input
-          type="text"
-          name="url"
-          id="url"
-        />
-      </div>
-      <button>Cadastrar</button>
-      <button>Cancelar</button>
-    </form>
+    <>
+      {
+        showButton && (
+          <button onClick={ showForm }>Cadastrar nova senha</button>
+        )
+      }
+
+      {newRegister && (
+        <form>
+          <div>
+            <label htmlFor="service">Nome do serviço</label>
+            <input
+              type="text"
+              name="service"
+              id="service"
+            />
+          </div>
+          <div>
+            <label htmlFor="login">Login</label>
+            <input
+              type="text"
+              name="login"
+              id="login"
+            />
+          </div>
+          <div>
+            <label htmlFor="senha">Senha</label>
+            <input
+              type="password"
+              name="senha"
+              id="senha"
+            />
+          </div>
+          <div>
+            <label htmlFor="url">URL</label>
+            <input
+              type="text"
+              name="url"
+              id="url"
+            />
+          </div>
+          <button>Cadastrar</button>
+          <button onClick={ cancelForm }>Cancelar</button>
+        </form>
+      )}
+    </>
+
   );
 }
 
